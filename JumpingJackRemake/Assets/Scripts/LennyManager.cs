c5fddb81648687c4d28b9acc87b1f1552cea9914
Rob Merrick <robmerrick181@gmail.com>
2021-06-19 09:51:55 -0600

    Updated the classic mode scene to finish the pixel perfect layout. Created more Lenny animations. Updated the font asset with the new changes for spacing.

--- JumpingJackRemake/Assets/Scripts/LennyManager.cs
+++ JumpingJackRemake/Assets/Scripts/LennyManager.cs
@@ -5,15 +5,46 @@ public class LennyManager : MonoBehaviour
     [SerializeField] private GameObject _lenny;
     [SerializeField] [Range(0.1F, 10.0F)] private float _runSpeed = 1.0F;
 
-    private void Update()
+    private SpriteRenderer _spriteRenderer;
+    private Animator _animator;
+    private bool _isJumping = false;
+
+	private void Start()
+	{
+        _spriteRenderer = _lenny.GetComponent<SpriteRenderer>();
+		_animator = _lenny.GetComponent<Animator>();
+	}
+
+	private void Update()
     {
-        if(Input.GetKey(KeyCode.RightArrow))
+        bool isRightArrowDown = Input.GetKey(KeyCode.RightArrow);
+        bool isLeftArrowDown = Input.GetKey(KeyCode.LeftArrow);
+
+        if(Input.GetKey(KeyCode.UpArrow))
 		{
-            _lenny.transform.Translate(Time.deltaTime * _runSpeed * Vector3.right);
+            _animator.SetTrigger("Jump");
+            _isJumping = true;
 		}
-        else if(Input.GetKey(KeyCode.LeftArrow))
+
+        if(!_isJumping)
 		{
-            _lenny.transform.Translate(Time.deltaTime * _runSpeed * Vector3.left);
+            if(isRightArrowDown && !isLeftArrowDown)
+            {
+                _animator.SetTrigger("Run");
+                _spriteRenderer.flipX = true;
+                _lenny.transform.Translate(Time.deltaTime * _runSpeed * Vector3.right);
+            }
+            else if(isLeftArrowDown && !isRightArrowDown)
+            {
+                _animator.SetTrigger("Run");
+                _spriteRenderer.flipX = false;
+                _lenny.transform.Translate(Time.deltaTime * _runSpeed * Vector3.left);
+            }
+            else
+            {
+                _animator.SetTrigger("Idle");
+                _spriteRenderer.flipX = false;
+            }
         }
     }
 }
