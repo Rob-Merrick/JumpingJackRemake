755d7c604d8618ab8363380a295c0680c2cc18b8
Rob Merrick <robmerrick181@gmail.com>
2021-06-24 18:32:15 -0600

    Finished the holes in the floor mesh! Hit the spacebar on this commit to spawn a new hole.

--- JumpingJackRemake/Assets/Scripts/FloorManager3D.cs
+++ JumpingJackRemake/Assets/Scripts/FloorManager3D.cs
@@ -1,3 +1,5 @@
+using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
 
 public class FloorManager3D : Manager<FloorManager3D>
@@ -11,25 +13,57 @@ public class FloorManager3D : Manager<FloorManager3D>
 	[SerializeField] private Floor3D _seventhFloor;
 	[SerializeField] private Floor3D _eigthFloor;
 
-	[SerializeField] private DiscMesh _disc1;
-	[SerializeField] private DiscMesh _disc2;
-	[SerializeField] private Hole3D _hole1;
-	[SerializeField] private Hole3D _hole2;
+	[SerializeField] private Hole3D _holePrefab;
+	[SerializeField] private DiscMesh _discPrefab;
+
+	private DiscMesh _floorNoHoles;
+	private List<Hole3D> _holes = new List<Hole3D>();
+	private readonly IDictionary<Hole3D, DiscMesh> _holeDiscMeshLookup = new Dictionary<Hole3D, DiscMesh>();
 
 	private float HoleSize => HoleManager3D.Instance.HoleSizeRadians;
 
-	private void Update()
+	private void Start()
 	{
-		SetHoleSize(_hole1, _hole2, _disc1);
-		SetHoleSize(_hole2, _hole1, _disc2);
+		_floorNoHoles = Instantiate(_discPrefab);
+		_floorNoHoles.ArcLength = 2.0F * Mathf.PI - 0.00001F;
 	}
 
-	//discMesh.ArcLength = 2.0F * Mathf.PI; //This is the fallback for no other holes.
+	private void Update()
+	{
+		if(Input.GetKeyDown(KeyCode.Space))
+		{
+			if(_floorNoHoles != null)
+			{
+				Destroy(_floorNoHoles.gameObject);
+			}
+			
+			Hole3D hole = Instantiate(_holePrefab);
+			hole.StartRotationRadians = Random.Range(0.0F, 2.0F * Mathf.PI);
+			hole.transform.SetParent(transform, worldPositionStays: false);
+			hole.MoveDirection = Random.Range(0.0F, 1.0F) <= 0.5F ? MoveAIDirection.LeftUp : MoveAIDirection.RightDown;
+			_holes.Add(hole);
+			_holeDiscMeshLookup.Add(hole, Instantiate(_discPrefab));
+		}
+
+		_holes = _holes.OrderBy(h => h.CurrentRotation).ToList();
+
+		for(int i = 0; i < _holes.Count; i++)
+		{
+			Hole3D hole1 = _holes[i];
+			Hole3D hole2 = _holes.Count > 1 ? _holes[(i + 1) % _holes.Count] : null;
+			SetHoleSize(hole1, hole2, _holeDiscMeshLookup[hole1]);
+		}
+	}
+
 	private void SetHoleSize(Hole3D hole, Hole3D otherHole, DiscMesh discMesh)
 	{
 		discMesh.StartingRadians = hole.CurrentRotation + HoleSize;
 
-		if(otherHole.CurrentRotation > hole.CurrentRotation)
+		if(otherHole == null)
+		{
+			discMesh.ArcLength = 2.0F * Mathf.PI - 0.00001F - HoleSize;
+		}
+		else if(otherHole.CurrentRotation > hole.CurrentRotation)
 		{
 			discMesh.ArcLength = Mathf.Clamp(otherHole.CurrentRotation - hole.CurrentRotation - HoleSize, 0.0F, 2.0F * Mathf.PI);
 		}
--- JumpingJackRemake/Assets/Scripts/Hole3D.cs
+++ JumpingJackRemake/Assets/Scripts/Hole3D.cs
@@ -3,19 +3,22 @@ using UnityEngine;
 public class Hole3D : MonoBehaviour
 {
     [SerializeField] private float _startRotationRadians = 0.0F;
-    [SerializeField] private MoveAIDirection _moveAIDirection = MoveAIDirection.LeftUp;
+    [SerializeField] private MoveAIDirection _moveDirection = MoveAIDirection.LeftUp;
     
-    public float CurrentRotation { get; private set; }
     public int FloorNumber { get; private set; } = 1;
+    public float CurrentRotation { get; private set; }
+    public float StartRotationRadians { get => _startRotationRadians; set => _startRotationRadians = value; }
+    public MoveAIDirection MoveDirection { get => _moveDirection; set => _moveDirection = value; }
 
 	private void Start()
 	{
 		CurrentRotation = _startRotationRadians;
-	}
+        CurrentRotation = Mathf.Repeat(CurrentRotation, 2.0F * Mathf.PI);
+    }
 
 	private void Update()
     {
-        CurrentRotation += _moveAIDirection == MoveAIDirection.LeftUp ? HoleManager3D.Instance.RotationalSpeedRadians * Time.deltaTime : -HoleManager3D.Instance.RotationalSpeedRadians * Time.deltaTime;
+        CurrentRotation += _moveDirection == MoveAIDirection.LeftUp ? HoleManager3D.Instance.RotationalSpeedRadians * Time.deltaTime : -HoleManager3D.Instance.RotationalSpeedRadians * Time.deltaTime;
         CurrentRotation = Mathf.Repeat(CurrentRotation, 2.0F * Mathf.PI);
     }
 }
